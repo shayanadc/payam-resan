585769cd308cd6de18c0d125ba847ee6d92a0faf
Faramarz Salehpour <subs@marz.co>
2017-01-16 13:19:01 +0330

    Add samples and configs

---
+++ tests/SampleClassTest.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: faramarz
+ * Date: 1/16/17
+ * Time: 13:15
+ */
+
+
+use FSalehpour\PHPPackageSkeleton\SampleClass;
+
+
+class SampleClassTest extends PHPUnit_Framework_TestCase
+{
+    $this->assertTrue(true);
+}
